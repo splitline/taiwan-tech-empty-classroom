197f8d147d1a7bfb0c0d8e2a3ae878209374e470
黃志仁 <tbsthitw@gmail.com>
2017-12-06 13:19:55 +0800

    debug (date error)

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, jsonify, request
 import requests
 from bs4 import BeautifulSoup
+import datetime
 app = Flask(__name__, static_url_path='/static')
 
 
@@ -11,9 +12,11 @@ def index():
 
 @app.route('/ajax', methods=['GET', 'POST'])
 def ajax():
+    today = datetime.date.today()
+    initial_day = datetime.date(2017,12,5)
     payload = {
-        "__EVENTTARGET": "classlist_ddl",
-        "__EVENTARGUMENT": "",
+        "__EVENTTARGET": "date_cal",
+        "__EVENTARGUMENT": 6548+(today-initial_day).days,
         "classlist_ddl": request.form['building'],
         "__VIEWSTATE": "dDw1NTk0MzU4NjE7dDw7bDxpPDE+Oz47bDx0PDtsPGk8Mz47aTw0Pjs+O2w8dDxAMDw7Ozs7Ozs7Ozs7Pjs7Pjt0PDtsPGk8NT47PjtsPHQ8QDA8cDxwPGw8U0Q7PjtsPGw8U3lzdGVtLkRhdGVUaW1lLCBtc2NvcmxpYiwgVmVyc2lvbj0xLjAuNTAwMC4wLCBDdWx0dXJlPW5ldXRyYWwsIFB1YmxpY0tleVRva2VuPWI3N2E1YzU2MTkzNGUwODk8MjAxNy0xMi0wNT47Pjs+Pjs+Ozs7Ozs7Ozs7Oz47Oz47Pj47Pj47Pj47Pu5S1476NkYk5hmd81mL76xisA4B",
         "__VIEWSTATEGENERATOR": "D2C5BC33"
